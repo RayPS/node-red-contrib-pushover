a04204a4ba8ef4c7f8b567ff9ae1941234d28704
Ray <ray@rayps.com>
2018-06-09 12:46:32 +0800

    Test

--- pushover.js
+++ pushover.js
@@ -1,44 +1,60 @@
 module.exports = function(RED) {
-    "use strict";
+    'use strict';
+    const request = require('request');
+    const fs = require('fs');
 
     function PushoverNode(n) {
         RED.nodes.createNode(this,n);
 
         this.title = n.title;
         var credentials = this.credentials;
-        if ((credentials) && (credentials.hasOwnProperty("token"))) { this.token = credentials.token; }
-        else { this.error("No Pushover api token set"); }
-        if ((credentials) && (credentials.hasOwnProperty("userKey"))) { this.userKey = credentials.userKey; }
-        else { this.error("No Pushover user key set"); }
+        if ((credentials) && (credentials.hasOwnProperty('token'))) { this.token = credentials.token; }
+        else { this.error('No Pushover api token set'); }
+        if ((credentials) && (credentials.hasOwnProperty('userKey'))) { this.userKey = credentials.userKey; }
+        else { this.error('No Pushover user key set'); }
+
+        var push = function(form){
+            request.post({ url: 'https://api.pushover.net/1/messages.json?html=1', formData: form }).on('error', function(err) {
+                this.error('Pushover error: ' + err);
+            });
+        };
 
         var node = this;
 
-        this.on("input",function(msg) {
-            msg.payload = typeof(msg.payload) === 'object' ? JSON.stringify(msg.payload) : msg.payload.toString()
-            if (msg.priority > 2 || msg.priority < -2) {
-                this.error("priority out of range")
-                msg.priority = 0
-            }
+        this.on('input',function(msg) {
+            msg.payload = typeof(msg.payload) === 'object' ? JSON.stringify(msg.payload) : msg.payload.toString();
+            if (msg.priority > 2 || msg.priority < -2) { node.error('priority out of range'); }
+
             let notification = {
-                "token"      : this.token,
-                "user"       : this.userKey,
-                "message"    : msg.payload,
-                "attachment" : msg.attachment || null,
-                "device"     : msg.device,
-                "title"      : this.title || msg.topic || "Node-RED",
-                "url"        : msg.url || null,
-                "url_title"  : msg.url_title || null,
-                "priority"   : msg.priority || 0,
-                "sound"      : msg.sound || null,
+                'token'      : node.token,
+                'user'       : node.userKey,
+                'message'    : msg.payload,
+                // 'attachment' : null,
+                'device'     : msg.device || null,
+                'title'      : node.title || msg.topic || 'Node-RED',
+                'url'        : msg.url || null,
+                'url_title'  : msg.url_title || null,
+                'priority'   : msg.priority || null,
+                'sound'      : msg.sound || null,
+            };
+
+            if (msg.attachment && msg.attachment.match(/^(\w+:\/\/)/igm)) {
+                request.get({url: urlfile, encoding: null}, function(error, response, body){
+                    fs.writeFileSync('/tmp/pushover-image', body);
+                    notification.attachment = fs.createReadStream('/tmp/pushover-image');
+                }).on('error', function(err) {
+                    node.error('Pushover error: ' + err);
+                });
+            } else {
+                notification.attachment = msg.attachment;
             }
             
-            this.warn(notification)
         });
     }
-    RED.nodes.registerType("pushover",PushoverNode,{
+    RED.nodes.registerType('pushover',PushoverNode,{
         credentials: {
-            userKey: {type:"text"},
-            token: {type: "text"}
+            userKey: {type:'text'},
+            token: {type: 'text'}
         }
     });
-}
+};
