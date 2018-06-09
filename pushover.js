8c2e582ff3f861227aba55cb3d39fbe940099c72
Ray <ray@rayps.com>
2018-06-09 13:07:59 +0800

    Test

--- pushover.js
+++ pushover.js
@@ -31,7 +31,7 @@ module.exports = function(RED) {
                 node.error('priority out of range');
             }
 
-            console.log('msg: ' + msg);
+            node.warn('msg: ' + msg);
 
             let notification = {
                 'token'      : node.token,
@@ -46,7 +46,7 @@ module.exports = function(RED) {
                 'sound'      : msg.sound || null,
             };
 
-            console.log('notification: ' + msg);
+            node.warn('notification: ' + msg);
 
             if (msg.attachment && msg.attachment.match(/^(\w+:\/\/)/igm)) {
                 request.get({url: msg.attachment, encoding: null}, function(error, response, body){
