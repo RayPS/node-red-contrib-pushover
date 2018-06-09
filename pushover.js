fd0b2320a062d98ee6a8ff17945daad7c87a8ba6
Ray <ray@rayps.com>
2018-06-09 15:30:47 +0800

    Test

--- pushover.js
+++ pushover.js
@@ -97,10 +97,10 @@ module.exports = function(RED) {
 
 
 
-    function RemoteServerNode(n) {
-        RED.nodes.createNode(this,n);
-        this.host = n.host;
-        this.port = n.port;
-    }
-    RED.nodes.registerType("remote-server",RemoteServerNode);
+    // function RemoteServerNode(n) {
+    //     RED.nodes.createNode(this,n);
+    //     this.host = n.host;
+    //     this.port = n.port;
+    // }
+    // RED.nodes.registerType("remote-server",RemoteServerNode);
 };
